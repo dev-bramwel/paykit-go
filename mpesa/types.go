9ef5f1d0ecf07ed615fdc711222558d51a4e5bc8
ochola-rich <richardochola3@gmail.com>
2026-07-18 21:10:40 +0300

    Task: Implemented the payment request and response for mpesa

--- mpesa/types.go
+++ mpesa/types.go
@@ -1,1 +1,16 @@
 package mpesa
+
+type STKPushRequest struct {
+	IdempotencyKey    string `json:"-"`
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	TransactionType   string `json:"TransactionType"`
+	Amount            int    `json:"Amount"`
+	PartyA            string `json:"PartyA"`
+	PartyB            string `json:"PartyB"`
+	PhoneNumber       string `json:"PhoneNumber"`
+	CallBackURL       string `json:"CallBackURL"`
+	AccountReference  string `json:"AccountReference"`
+	TransactionDesc   string `json:"TransactionDesc"`
+}
